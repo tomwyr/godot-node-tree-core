c472c7f5d541ed4ecc3d8a90499068e1677058df
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-07-26 01:19:55 +0200

    Set prepare binary script variables in env

--- scripts/prepare-binary.sh
+++ scripts/prepare-binary.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-source_path=.build/release/${{ matrix.file_name }}${{ matrix.file_ext }}
-out_path=.build/godotNodeTreeCore-${{ github.ref_name }}-${{ matrix.platform }}${{ matrix.file_ext }}
+source_path=.build/release/$LIB_FILE_NAME$LIB_EXTENSION
+out_path=.build/libGodotNodeTreeCore-$VERSION-$TARGET_PLATFORM$LIB_EXTENSION
 cp $source_path $out_path
 chmod +x $out_path
 echo "out_path=$out_path" >> $GITHUB_OUTPUT
